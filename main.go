3069dcea5c551f56fdb2b29e6ca8451c1dea2020
Roberto Fernandez Toro <robertobouses@gmail.com>
2023-02-06 23:48:22 +0100

    Tareas funcionando

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Task struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	DueDate     string `json:"dueDate"`
+}
+
+var taskList []Task
+
+func PostTask(c *gin.Context) {
+	var task Task
+
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	task.ID = len(taskList) + 1
+	taskList = append(taskList, task)
+	fmt.Println(taskList)
+
+	c.JSON(http.StatusCreated, task)
+
+}
 
 func main() {
-	fmt.Println("Hola mundo")
+
+	r := gin.Default()
+	r.POST("/task", PostTask)
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
