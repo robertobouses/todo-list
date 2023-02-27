f6d7dc45941b77d497111d48ed8301c37fe42456
Roberto Fernandez Toro <robertobouses@gmail.com>
2023-02-27 02:13:20 +0100

    Second Data Base SQL

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
 type Task struct {
@@ -19,7 +19,7 @@ type Task struct {
 
 func main() {
 	// Abrir la conexión con la base de datos
-	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/database_name")
+	db, err := sql.Open("postgres", "postgresql://postgres:mypassword@localhost:5432/database_name?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,28 +40,21 @@ func main() {
 		}
 
 		// Insertar la tarea en la base de datos
-		stmt, err := db.Prepare("INSERT INTO tasks(title, description, due_date) VALUES (?, ?, ?)")
+		stmt, err := db.Prepare("INSERT INTO tasks(title, description, due_date) VALUES ($1, $2, $3) RETURNING id")
 		if err != nil {
 			c.AbortWithError(http.StatusInternalServerError, err)
 			return
 		}
 		defer stmt.Close()
 
-		result, err := stmt.Exec(task.Title, task.Description, task.DueDate)
-		if err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-
-		// Obtener el ID generado por la base de datos
-		id, err := result.LastInsertId()
-		if err != nil {
+		var id int
+		if err := stmt.QueryRow(task.Title, task.Description, task.DueDate).Scan(&id); err != nil {
 			c.AbortWithError(http.StatusInternalServerError, err)
 			return
 		}
 
 		// Actualizar el ID de la tarea y devolverla como respuesta
-		task.ID = int(id)
+		task.ID = id
 		c.JSON(http.StatusOK, task)
 	})
 
@@ -73,13 +66,13 @@ func main() {
 
 func createTable(db *sql.DB) {
 	query := `
-        CREATE TABLE IF NOT EXISTS tasks (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            title VARCHAR(255) NOT NULL,
-            description TEXT,
-            due_date DATE
-        );
-    `
+		CREATE TABLE IF NOT EXISTS tasks (
+			id SERIAL PRIMARY KEY,
+			title VARCHAR(255) NOT NULL,
+			description TEXT,
+			due_date DATE
+		);
+	`
 	if _, err := db.Exec(query); err != nil {
 		log.Fatal(err)
 	}
