5910bb7ba023fbf79e404d6c2659a88ceac0f670
Roberto Fernandez Toro <robertobouses@gmail.com>
2023-03-07 00:09:14 +0100

    Expired Tasks

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
@@ -117,9 +118,31 @@ func main() {
 		c.JSON(http.StatusOK, tasks)
 	})
 
-	// Obtener todas las tareas completadas
+	// Obtener todas las tareas no completadas
 	r.GET("/tasks/pending", func(c *gin.Context) {
 		rows, err := db.Query("SELECT id, title, description, due_date, completed FROM tasks WHERE completed=false ORDER BY id")
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		defer rows.Close()
+
+		tasks := []Task{}
+		for rows.Next() {
+			var task Task
+			if err := rows.Scan(&task.ID, &task.Title, &task.Description, &task.DueDate, &task.Completed); err != nil {
+				c.AbortWithError(http.StatusInternalServerError, err)
+				return
+			}
+			tasks = append(tasks, task)
+		}
+
+		c.JSON(http.StatusOK, tasks)
+	})
+
+	// Obtener todas las tareas con fecha expirada
+	r.GET("/tasks/expired", func(c *gin.Context) {
+		rows, err := db.Query("SELECT id, title, description, due_date, completed FROM tasks WHERE due_date<$1 ORDER BY id", time.Now())
 		if err != nil {
 			c.AbortWithError(http.StatusInternalServerError, err)
 			return
