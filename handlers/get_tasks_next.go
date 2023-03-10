a3f333dd4ca37d4d921df26c1cf819b426c30e95
Roberto Fernandez Toro <robertobouses@gmail.com>
2023-03-11 00:02:00 +0100

    PR changes

--- handlers/get_tasks_next.go
+++ handlers/get_tasks_next.go
@@ -3,6 +3,9 @@ package handlers
 import (
 	"database/sql"
 	"log"
+	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,10 +18,21 @@ func GetTasksNext(c *gin.Context) {
 	if limit == "" {
 		limit = "10"
 	}
-
-	rows, err := db.Query("SELECT * FROM tasks WHERE completed=false AND due_date<=NOW() ORDER BY due_date ASC LIMIT $1", limit)
+	daysAheadStr := c.Query("daysAhead")
+	daysAhead, err := strconv.Atoi(daysAheadStr)
 	if err != nil {
-		log.Fatal(err)
+		daysAhead = 7 // usar un valor predeterminado si daysAhead no se puede convertir a entero
+	}
+
+	now := time.Now()                      // fecha actual
+	future := now.AddDate(0, 0, daysAhead) // fecha futura (por ejemplo, dentro de 7 días)
+
+	rows, err := db.Query("SELECT * FROM tasks WHERE completed=false AND due_date BETWEEN $1 AND $2 ORDER BY due_date ASC LIMIT $3", now, future, limit)
+
+	if err != nil {
+		log.Printf("error querying for next tasks: %v \n", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
 	}
 	defer rows.Close()
 
@@ -27,12 +41,17 @@ func GetTasksNext(c *gin.Context) {
 		var task Task
 		err := rows.Scan(&task.ID, &task.Title, &task.Description, &task.DueDate, &task.Completed)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("error querying for next tasks: %v \n", err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
 		}
 		tasks = append(tasks, task)
 	}
 	if err := rows.Err(); err != nil {
-		log.Fatal(err)
+		log.Printf("error querying for next tasks: %v \n", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
 	}
+
 	c.JSON(200, tasks)
 }
